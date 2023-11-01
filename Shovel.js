59c54a739fa61b9e1333fb010965740c4fb7ff99
jurchiks33 <jurisslisans@gmail.com>
2023-11-01 12:16:44 +0100

    work on a sparks

--- Shovel.js
+++ Shovel.js
@@ -1,5 +1,7 @@
 //shovel constructor
 function Shovel() {
+    this.sparks = [];
+
 	this.draw = function() {
 		fill(150);
 		rect(mouseX - 5, mouseY - 30, 10, 80);
@@ -10,6 +12,19 @@ function Shovel() {
         vertex(mouseX + 20, mouseY - 50);
         vertex(mouseX - 20, mouseY - 50);
         endShape(CLOSE);
+
+        for (let i = this.sparks.lenght - 1; i >= 0; i --) {
+            let spark = this.sparks[i];
+            fill(spark.color);
+            ellipse(spark.x, spark.y, spark.size);
+            spark.x += spark.vx;
+            spark.y += spark.vy;
+            spark.vy += spark.gravity;
+            spark.size *= 0.95;
+            if (spark.size < 0.5) {
+                this.sparks.splice(i, 1);
+            }
+        }
 	}
 
 	this.hitZombies = function(zombies) {
